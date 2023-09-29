ffeddc5bad4e58431143c409ac97d88ae1cdb07d
dirlei-costa.com <dirleisc@hotmail.com>
2023-09-29 16:13:52 -0300

    :package: calculo de orcamnrto

--- js.js
+++ js.js
@@ -1,3 +1,31 @@
 document.querySelector('.hamburguer').addEventListener('click',() =>
   document.querySelector('.container').classList.toggle('show-menu')
-)
+);
+
+document.querySelector('#qty').addEventListener('change', newPrice)
+document.querySelector('#js').addEventListener('change', newPrice)
+document.querySelector('#layout-ok').addEventListener('change', newPrice)
+document.querySelector('#layout-no').addEventListener('change', newPrice)
+document.querySelector('#time').addEventListener('change', function (){
+  const time = document.querySelector('#time').value
+  document.querySelector('label[for=time]').innerHTML = `Prazo: ${time} semanas`
+  newPrice()
+})
+
+function newPrice(){
+  const qty = document.querySelector('#qty').value
+  const yesJS = document.querySelector('#js').checked
+  const yesLayout = document.querySelector('#layout-ok').checked
+  const time = document.querySelector('#time').value
+
+  let price = qty * 100
+  if(yesJS) price *=  1.1
+  // price = price + (price * 10/100)
+  //price = price * 1.1
+  if(yesLayout) price = price + 500
+  let taxaUrgente = 1 - time * 0.1;
+  price *= 1 + taxaUrgente
+
+
+  document.querySelector('#price').innerHTML = `R$ ${price.toFixed(2)}`;
+}
